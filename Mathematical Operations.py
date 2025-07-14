f60e4530df90b5c8ba1b17c7a269c794d8ae3943
Chetan S. Chavan <chetan.chvn2@gmail.com>
2025-07-14 12:04:24 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+Add = num1 + num2
+Sub = num1 - num2
+Mul = num1 * num2
+Div = num1 / num2
+
+print("Addition: ", Add)
+print("Substraction: ", Sub)
+print("Multiplication: ", Mul)
+print("Division: ", Div)
---
+++ Personalized Greeting.py
@@ -0,0 +1,8 @@
+fname = input("Enter the firstname: ")
+lname = input("Enter the lastname: ")
+
+#Concat fname & lname
+fullname = fname + " " + lname
+
+#Personalized greeting message using the full name
+print("Hello, " + fullname + "!" + " Welcome to the Python program tutorial.")
